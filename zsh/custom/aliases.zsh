148153cbc0f643ee7cdf85faa2b62ede5d5ee3ca
owdsu <a@owd.su>
2016-06-24 16:28:01 +0300

    Change alias svim

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -5,8 +5,4 @@ alias -g H='|head'
 alias -g T='|tail'
 alias -g L='|less'
 
-if [ -f '/root/.vimrc-it' ]; then
-    alias svim='sudo vim -u /root/.vimrc-it'
-else
-    alias svim='sudo vim'
-fi
+alias svim='sudo -E vim'
